15b152705dcaeb151837dfa7faeb22711f3bfb30
Karine Salbueno <karine.costa@somagrupo.com.br>
2022-04-23 12:21:04 -0300

    lê os dados do input

--- src/App.js
+++ src/App.js
@@ -29,10 +29,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseDataHandler = expense => {
+    console.log('in App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
       <header className="App-header">
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseDataHandler} />
         <Expenses items={expenses} />
 
       </header>
--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -1,32 +1,79 @@
 import { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, SetEnteredTitle] = useState('');
-            
-    const titleChangeHandler = (event) =>{
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
         SetEnteredTitle(event.target.value)
     }
-    return <form>
-        <div className='new-expense__controls'>
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
 
-            <div className='new-expense__control'>
-                <label>Tittle</label>
-                <input type="text" onChange={titleChangeHandler} />
+    const submitHandler = (event) => {
+        event.preventDefault();
+        //evita que a pag seja recarregada ao enviar o formulário
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+
+       props.onSaveExpenseData(expenseData)
+    /* pega a propriedade do newExpense gerando um id aleatorio e adicionando ao 
+    componente ExpenseForm chamado e atribuindo a ele qnd o submit é feito*/
+    /*passa o expenseData dentro para que possa unir os dados de id c titulo, preço e data */
+    
+
+        SetEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+
+                <div className='new-expense__control'>
+                    <label>Tittle</label>
+                    <input
+                        type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler} />
+                </div>
+
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input
+                        type="number" min="0.01" step="0.01"
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}/>
+                </div>
+
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input
+                        type="date" min="2020-01-01" max="2022-05-03"
+                        value={enteredDate} 
+                        onChange={dateChangeHandler} />
+                </div>
+
             </div>
-            <div className='new-expense__control'>
-                <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+            <div className='new-expense__actions'>
+                <button>Add expense</button>
             </div>
-            <div className='new-expense__control'>
-                <label>Date</label>
-                <input type="date" min="2020-01-01" max="2022-05-03" />
-            </div>
-        </div>
-        <div className='new-expense__actions'>
-            <button>Add expense</button>
-        </div>
-    </form>
+
+        </form>
+    )
+    //adicionar o VALUE dentro do input, faz com que limpe a entrada do input após submeter o formulário,
+    //p.s: para isso é necessário colocar o setState ('') novamente após o resultado q vc quer
 }
 
 export default ExpenseForm
